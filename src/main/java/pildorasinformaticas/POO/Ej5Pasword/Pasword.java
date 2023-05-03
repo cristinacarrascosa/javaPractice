742c9d770f79060dea47097eb379528cc0e4d68b
cristinacarrascosa <cctausias@gmail.com>
2023-05-03 21:12:29 +0200

    falta hacer el ejercicio que está entre el video 54 y 55

---
+++ src/main/java/pildorasinformaticas/POO/Ej5Pasword/Pasword.java
@@ -0,0 +1,18 @@
+package pildorasinformaticas.POO.Ej5Pasword;
+
+import pildorasinformaticas.POO.Ej4ColegioAlumno.Alumno;
+
+public class Pasword {
+
+    private int longPassword;
+
+    private String[] arrayPaswords;
+
+    private int longArray;
+
+    public Pasword (int longPassword, int longArray){
+
+        this.longArray = longArray;
+        this.longPassword = longPassword;
+    }
+}
---
+++ src/main/java/pildorasinformaticas/POO/Ej5Pasword/UsoPassword.java
@@ -0,0 +1,7 @@
+package pildorasinformaticas.POO.Ej5Pasword;
+
+public class UsoPassword {
+    public static void main(String[] args) {
+
+    }
+}
